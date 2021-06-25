2d2be553795e2e9b905f2f44e5f0059668fff9ec
vrkknn <frederico@dsuite.com.br>
2021-06-25 00:03:14 -0300

    Ask for user input between stages

--- Pymodoro/main.py
+++ Pymodoro/main.py
@@ -1,6 +1,6 @@
 import time, os, sys
 
-version = "v0.2"
+version = "v0.3"
 
 # TODO
 # - remove cursor from countdown
@@ -34,25 +34,33 @@ def pomodoro():
             mins = t // 60
             secs = t % 60
             timer = f"{mins}:{secs}"
-            print(timer, end="\r")
+            print(timer, end="\r") # \r replaces last line
             time.sleep(1)
             t -= 1
         print('\a') # terminal bell
 
-        # break
-        clear()
-        if breaks == 3: t = 10 * 60 # 10 minutes on the 4th break
-        else: t = 5 * 60 # 5 minutes
-        print("Pymodoro " + version + "\n\nTotal breaks: " + str(breaks) + "\n\nBreak time")
-        while t: 
-            mins = t // 60 
-            secs = t % 60
-            timer = f"{mins}:{secs}"
-            print(timer, end="\r")
-            time.sleep(1)
-            t -= 1 
-        breaks += 1
-        print('\a') # terminal bell
+        if breaks == 3: choice = input("Long rest? [y/n] ")
+        else: choice = input("Short rest? [y/n] ")
+
+        if choice.lower() == "y" or choice.lower() == "yes":
+            # break
+            clear()
+            if breaks == 3: t = 10 * 60 # 10 minutes on the 4th break
+            else: t = 5 * 60 # 5 minutes
+            print("Pymodoro " + version + "\n\nTotal breaks: " + str(breaks) + "\n\nBreak time")
+            while t: 
+                mins = t // 60 
+                secs = t % 60
+                timer = f"{mins}:{secs}"
+                print(timer, end="\r")
+                time.sleep(1)
+                t -= 1 
+            breaks += 1
+            print('\a') # terminal bell
+            choice = input("Back to work? [y/n] ")
+            if choice.lower() == "y" or choice.lower() == "yes": continue
+            else: break
+        else: break
 
 if __name__ == "__main__":
     main()
